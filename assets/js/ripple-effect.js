edb945d5cc6ac87d536b3d7748a2181d65b6eb3d
7thSinScribe <131284458+7thSinScribe@users.noreply.github.com>
2023-04-23 17:40:26 +0100

    Reswag

--- assets/js/ripple-effect.js
+++ assets/js/ripple-effect.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const rippleText = document.querySelector(".ripple-text");
   if (rippleText) {
-    const text = rippleText.textContent;
+    const text = rippleText.textContent.trim(); // Remove leading/trailing whitespace
     let newText = "";
     for (let i = 0; i < text.length; i++) {
-      newText += `<span>${text[i]}</span>`;
+      if (text[i] !== " ") { // Skip whitespace characters
+        newText += `<span>${text[i]}</span>`;
+      }
     }
     rippleText.innerHTML = newText;
   }
 });
+
